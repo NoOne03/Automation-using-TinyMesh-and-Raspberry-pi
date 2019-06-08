23f853a519ddee17cfb2e04b6dd54e9b20866c1d
Prateek Kr Singh <44129531+NoOne03@users.noreply.github.com>
2019-06-08 11:34:32 +0530

    Update status.py

--- status.py
+++ status.py
@@ -9,6 +9,10 @@ while True:
         ch = port.read(1)
         data.append(ch)
     if (len(data)>30):
+        #print received data to console
+        print(data)
+        # or store the data to database for remote access of status
+        #here only change in status of device is stored to database
         if ((data[5:9]== d1) and (data[19:20]=='\x40') and (data[26:27]=='\x81')):
             try:#Open database connection
                 db = MySQLdb.connect("IP_address","Database_Username","Database_Password","database_name" )
